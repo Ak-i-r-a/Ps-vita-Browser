5b5b2becf8871b945e985bcd22602849571c3fa5
Akira <iletait14fois@gmail.com>
2025-01-15 12:51:09 +0100

    toutoutzemoune

--- download.sh
+++ download.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Dossier où les dépendances seront téléchargées
-DEPENDENCIES_DIR=$(pwd)
-echo "Téléchargement des dépendances dans le dossier : $DEPENDENCIES_DIR"
-
 # Téléchargement de Firefox
 FIREFOX_URL="https://download.mozilla.org/?product=firefox-134.0.1-ssl&os=linux64&lang=en-US"
 FIREFOX_ARCHIVE="firefox-134.0.1.tar.bz2"
 
-if [ ! -f "$DEPENDENCIES_DIR/$FIREFOX_ARCHIVE" ]; then
+if [ ! -f "$FIREFOX_ARCHIVE" ]; then
     echo "Téléchargement de Firefox..."
-    wget -O "$DEPENDENCIES_DIR/$FIREFOX_ARCHIVE" "$FIREFOX_URL"
+    wget -O "$FIREFOX_ARCHIVE" "$FIREFOX_URL"
+    tar -xjf "$FIREFOX_ARCHIVE"
 else
     echo "Firefox déjà téléchargé."
 fi
@@ -19,31 +16,33 @@ fi
 NOVNC_REPO="https://github.com/novnc/noVNC/archive/refs/heads/master.zip"
 NOVNC_ARCHIVE="novnc-master.zip"
 
-if [ ! -f "$DEPENDENCIES_DIR/$NOVNC_ARCHIVE" ]; then
+if [ ! -f "$NOVNC_ARCHIVE" ]; then
     echo "Téléchargement de noVNC..."
-    wget -O "$DEPENDENCIES_DIR/$NOVNC_ARCHIVE" "$NOVNC_REPO"
+    wget -O "$NOVNC_ARCHIVE" "$NOVNC_REPO"
+    unzip "$NOVNC_ARCHIVE"
 else
     echo "noVNC déjà téléchargé."
 fi
 
 # Téléchargement de websockify
-WEBSOCKIFY_REPO="https://github.com/novnc/websockify/archive/refs/heads/main.zip"
-WEBSOCKIFY_ARCHIVE="websockify-main.zip"
+WEBSOCKIFY_REPO="https://github.com/novnc/websockify.git"
+WEBSOCKIFY_DIR="websockify"
 
-if [ ! -f "$DEPENDENCIES_DIR/$WEBSOCKIFY_ARCHIVE" ]; then
+if [ ! -d "$WEBSOCKIFY_DIR" ]; then
     echo "Téléchargement de websockify..."
-    wget -O "$DEPENDENCIES_DIR/$WEBSOCKIFY_ARCHIVE" "$WEBSOCKIFY_REPO"
+    git clone "$WEBSOCKIFY_REPO" "$WEBSOCKIFY_DIR"
 else
     echo "websockify déjà téléchargé."
 fi
 
 # Téléchargement de PulseAudio
-PULSEAUDIO_URL="https://freedesktop.org/software/pulseaudio/releases/pulseaudio-16.1.tar.xz"
-PULSEAUDIO_ARCHIVE="pulseaudio-14.2.tar.gz"
+PULSEAUDIO_URL="https://freedesktop.org/software/pulseaudio/releases/pulseaudio-17.0.tar.gz"
+PULSEAUDIO_ARCHIVE="pulseaudio-17.0.tar.gz"
 
-if [ ! -f "$DEPENDENCIES_DIR/$PULSEAUDIO_ARCHIVE" ]; then
+if [ ! -f "$PULSEAUDIO_ARCHIVE" ]; then
     echo "Téléchargement de PulseAudio..."
-    wget -O "$DEPENDENCIES_DIR/$PULSEAUDIO_ARCHIVE" "$PULSEAUDIO_URL"
+    wget -O "$PULSEAUDIO_ARCHIVE" "$PULSEAUDIO_URL"
+    tar -xzf "$PULSEAUDIO_ARCHIVE"
 else
     echo "PulseAudio déjà téléchargé."
 fi
@@ -52,9 +51,10 @@ fi
 XORG_URL="https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-1.20.14.tar.gz"
 XORG_ARCHIVE="xorg-server-1.20.14.tar.gz"
 
-if [ ! -f "$DEPENDENCIES_DIR/$XORG_ARCHIVE" ]; then
+if [ ! -f "$XORG_ARCHIVE" ]; then
     echo "Téléchargement de Xorg..."
-    wget -O "$DEPENDENCIES_DIR/$XORG_ARCHIVE" "$XORG_URL"
+    wget -O "$XORG_ARCHIVE" "$XORG_URL"
+    tar -xzf "$XORG_ARCHIVE"
 else
     echo "Xorg déjà téléchargé."
 fi
